8523fa846118059fe4d750958652c6e156bd102f
Julie Evans <juliemevans17@gmail.com>
2023-08-17 15:04:25 -0500

    Update pokemon border radius

--- config/development.ts
+++ config/development.ts
@@ -1,7 +1,7 @@
 import type { Config } from './types';
 
 export const Development: Config = {
-  API_HOST: 'https://staging.pokedextracker.com/api',
+  API_HOST: 'https://pokedextracker.com/api',
   ENVIRONMENT: 'development',
   GA_ID: 'UA-45307701-5',
   ROLLBAR_TOKEN: '3695370c334447daa140a0d0e902360b',
