1ab07fce598a69d5592aa7f006d500e8e000aa50
Julie Evans <juliemevans17@gmail.com>
2023-08-26 18:27:41 -0500

    Update friend code style

--- app/components/library/FriendCode.tsx
+++ app/components/library/FriendCode.tsx
@@ -1,6 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+// import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import EditIcon from '@mui/icons-material/Edit';
 import { useParams } from 'react-router';
 
 import { ReactGA } from '../../utils/analytics';
@@ -8,6 +9,7 @@ import { useSession } from '../../hooks/contexts/use-session';
 import { useUser } from '../../hooks/queries/users';
 
 import type { ReactNode } from 'react';
+import { IconButton, Stack, Typography } from '@mui/material';
 
 export function FriendCode () {
   const { username } = useParams<{ username: string }>();
@@ -23,18 +25,35 @@ export function FriendCode () {
     const handleClick = () => ReactGA.event({ action: 'click edit friend code', category: 'User' });
 
     editAccountBtn = (
-      <Link onClick={handleClick} to="/account"><FontAwesomeIcon icon={faPencilAlt} /></Link>
+      // <Link onClick={handleClick} to="/account"><FontAwesomeIcon icon={faPencilAlt} /></Link>
+      <IconButton aria-label="edit" component={Link} onClick={handleClick} size="small" to="/account">
+        <EditIcon fontSize="small" />
+      </IconButton>
     );
   }
 
   return (
-    <div>
-      <h2>
-        <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span> {editAccountBtn}
-      </h2>
-      <h2>
-        <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span> {editAccountBtn}
-      </h2>
-    </div>
+    // <div>
+    //   <h2>
+    //     <b>3DS FC</b>: <span className={user.friend_code_3ds ? '' : 'fc-missing'}>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span> {editAccountBtn}
+    //   </h2>
+    //   <h2>
+    //     <b>Switch FC</b>: <span className={user.friend_code_switch ? '' : 'fc-missing'}>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span> {editAccountBtn}
+    //   </h2>
+    // </div>
+    <Stack direction="column">
+      <Stack alignItems="center" direction="row">
+        <Typography sx={{ pt: 0.2, span: { color: user.friend_code_3ds ? '' : 'text.disabled' } }}>
+          <b>3DS FC</b>: <span>{user.friend_code_3ds || 'XXXX-XXXX-XXXX'}</span>
+        </Typography>
+        {editAccountBtn}
+      </Stack>
+      <Stack alignItems="center" direction="row">
+        <Typography sx={{ pt: 0.2, span: { color: user.friend_code_switch ? '' : 'text.disabled' } }}>
+          <b>Switch FC</b>: <span>{user.friend_code_switch || 'SW-XXXX-XXXX-XXXX'}</span>
+        </Typography>
+        {editAccountBtn}
+      </Stack>
+    </Stack>
   );
 }
