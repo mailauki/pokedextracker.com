f913f184bce301102ba8b1c13f50db960c0d6723
Julie Evans <juliemevans17@gmail.com>
2023-09-09 12:35:26 -0500

    Update alert

--- app/components/library/Alert.tsx
+++ app/components/library/Alert.tsx
@@ -2,6 +2,8 @@ import classNames from 'classnames';
 
 import type { ReactNode } from 'react';
 
+import { Alert as Alerts } from '@mui/material';
+
 interface Props {
   className?: string;
   message: ReactNode;
@@ -14,8 +16,9 @@ export function Alert ({ className, message, type }: Props) {
   }
 
   return (
-    <div className={classNames('alert', `alert-${type}`, className)} style={{ borderRadius: '4px' }}>
-      {message}
-    </div>
+    // <div className={classNames('alert', `alert-${type}`, className)} style={{ borderRadius: '4px' }}>
+    //   {message}
+    // </div>
+    <Alerts severity={type}>{message}</Alerts>
   );
 }
